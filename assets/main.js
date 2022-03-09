ce5923fdfbd778ec840e151682a6470cd2f947dd
RyanAltamont <97465962+RyanAltamont@users.noreply.github.com>
2022-03-09 09:56:36 -0600

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+// Complete the questions below; we'll go over these at our next class meeting.
+// The skills covered in these questions are DIRECTLY related to the notes sheet
+// called Basics of JS.
+
+// 1. Declare a variable whose value CANNOT change called `favCourse`
+  const favCourse = `Math`;
+// Note: This variable should be a string.
+
+// 2. Log the value of `favCourse` to the console
+console.log(favCourse);
+// 3. Create a variable whose value CAN change called `grade`
+  let grade = `50%`;
+// Note: this variable should be an integer
+
+// 4. Log the value of `grade` to the console
+    console.log(`50%`);
+// 5. Create a variable whose value is a Boolean called `isHope`
+  let isHope = false;
+// You should set it dependent upon if there is/is not hope for your grade
+// in said course.
+
+// 6. Log the value of `isHope` to the console.
+  console.log(false);
+// 7. Write a statement using the format below wherein your variables are "plugged in"
+
+  console.log(`My favorite course is ${favCourse} and my grade is ${grade}`);
+// The console should read: My favorite course is Geography. My grade is a 98. Hope: true.
+// If you're drawing a blank on this skill, this documentation will help:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
